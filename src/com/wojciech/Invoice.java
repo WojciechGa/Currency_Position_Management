3ba19adaedd69b0a00cddcaf03d3f07a166115b5
Wojciech <wg117@wp.pl>
2019-11-18 19:58:11 +0100

    HeadContract and Invoice class Builder added

--- src/com/wojciech/Invoice.java
+++ src/com/wojciech/Invoice.java
@@ -22,39 +22,6 @@ public class Invoice implements ShowDate{
     private InvoiceType invoiceType;
     private double kursNbpPodatkowy;
 
-    /*
-      Initializes date fields
-     */
-    {
-        issueDate = Calendar.getInstance();
-        paymentDate = Calendar.getInstance();
-    }
-
-    /**
-     *  Constructing invoice, NBP rate is taken from tables (to be set)
-     *  isueDate is on day of registration
-     *  payment day hour is on 23:59:59
-     * @param currency of invoice
-     * @param amount in currency
-     * @param year of payment
-     * @param month  of payment
-     * @param day of payment
-     * @param invoiceType asset or liability (Enum type)
-     */
-    public Invoice(String currency, double amount, int year, int month, int day, InvoiceType invoiceType) {
-        this.currency = currency;
-        this.amount = amount;
-        paymentDate.set(year, month, day, 23, 59,59);
-        this.invoiceType = invoiceType;
-    }
-
-    public Invoice (String currency, double amount, int year, int month, int day, InvoiceType invoiceType,
-                    int yearStart, int monthStart, int dayStart){
-        this(currency, amount, year, month, day, invoiceType);
-        issueDate.set(yearStart, monthStart, dayStart);
-    }
-
-
     @Override
     public String toString() {
         return "Invoice\n" +
@@ -65,5 +32,66 @@ public class Invoice implements ShowDate{
                 ",\n invoiceType=" + invoiceType +
 //                "\n, kursNbpPodatkowy=" + kursNbpPodatkowy +
                 '}';
+    }
+
+    public static class Builder{
+
+        //obligatory fields
+        private String currency;
+        private double amount;
+        private Calendar paymentDate;
+        private InvoiceType invoiceType;
+
+        //optional fields
+        private Calendar issueDate;
+        private double kursNbpPodatkowy = 0;
+
+
+        // Initializes date fields
+         {
+            issueDate = Calendar.getInstance();
+            paymentDate = Calendar.getInstance();
+        }
+
+        /**
+         *  Constructing invoice, NBP rate is taken from tables (to be set)
+         *  isueDate is on day of registration
+         *  payment day hour is on 23:59:59
+         * @param currency of invoice
+         * @param amount in currency
+         * @param year of payment
+         * @param month  of payment
+         * @param day of payment
+         * @param invoiceType asset or liability (Enum type)
+         */
+        public Builder(String currency, double amount, int year, int month, int day, InvoiceType invoiceType) {
+            this.currency = currency;
+            this.amount = amount;
+            paymentDate.set(year, month, day, 23, 59,59);
+            this.invoiceType = invoiceType;
+        }
+
+        public Builder issueDate(int year, int month, int day){
+            issueDate.set(year, month - 1, day);
+            return this;
+        }
+
+        public Builder kursNbpPodatkowy(double kursNbpPodatkowy){
+            this.kursNbpPodatkowy = kursNbpPodatkowy;
+            return this;
+        }
+
+        public Invoice build(){
+            return new Invoice(this);
+        }
+    }
+
+    private Invoice(Builder builder){
+        this.currency = builder.currency;
+        this.amount = builder.amount;
+        this.paymentDate = builder.paymentDate;
+        this.invoiceType = builder.invoiceType;
+        this.issueDate = builder.issueDate;
+        this.kursNbpPodatkowy = builder.kursNbpPodatkowy;
     }
 }
